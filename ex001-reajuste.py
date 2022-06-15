ebb4c12979e09f842d4c54a6f798459eec2fa605
Aluno Cyber <aluno.cyber@fbter.org.br>
2022-06-15 11:38:11 -0300

    reajuste

---
+++ ex001-reajuste.py
@@ -0,0 +1,17 @@
+print("Reajuste salarial: ")
+print("(1) = [2019: 3,43%]")
+print("(2) = [2018: 2,07%]")
+print("(3) = [2017: 6,58%]\n")
+a = int(input("Escolha seu reajuste: "))
+salario = float(input("Qual o seu sálario? "))
+if (a == 1):
+    reajuste = salario*0.0343 + salario
+    print(reajuste)
+elif (a == 2):
+    reajuste = salario*0.0207 + salario
+    print(reajuste)
+elif (a == 3):
+    reajuste = salario*0.0658 + salario
+    print("oi")
+else: 
+    print("digite uma opção de reajuste válida!")
